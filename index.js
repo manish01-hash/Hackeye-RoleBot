1fffefc3b9c9299985bc859327965c0174862632
manish01-hash <manishdhaygude36@gmail.com>
2025-04-15 19:11:43 +0530

    update

--- index.js
+++ index.js
@@ -52,3 +52,9 @@ client.on('guildMemberUpdate', async (oldMember, newMember) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is running.'));
+app.listen(3000, () => console.log('✅ Express server running on port 3000'));
